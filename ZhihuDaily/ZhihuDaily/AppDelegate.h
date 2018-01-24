3954a51ce357f739227165a3e2ca87369cf2feb2
Jerry <huangjun0529@126.com>
2018-01-24 15:03:47 +0800

    项目托管到github

---
+++ ZhihuDaily/ZhihuDaily/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ZhihuDaily
+//
+//  Created by hjun on 2018/1/24.
+//  Copyright © 2018年 hjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
